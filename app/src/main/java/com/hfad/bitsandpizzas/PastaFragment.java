3b3d50ec751c36b1afda08e72402fd8e260549a3
Chris <cciric44@gmail.com>
2020-04-22 23:05:24 -0400

    added a recycler view for the pizzas tab

--- app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
+++ app/src/main/java/com/hfad/bitsandpizzas/PastaFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +31,10 @@ public class PastaFragment extends ListFragment {
                 getResources().getStringArray(R.array.pasta));
         setListAdapter(adapter);
         return super.onCreateView(inflater, container, savedInstanceState);
+
+
+
+
     }
 
 }
--- app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
+++ app/src/main/java/com/hfad/bitsandpizzas/PizzaFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +17,7 @@ import android.widget.ArrayAdapter;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PizzaFragment extends ListFragment {
+public class PizzaFragment extends Fragment {
 
 
     public PizzaFragment() {
@@ -26,10 +28,28 @@ public class PizzaFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        //this code we used when this class extended the list fragment class
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1,
+//                getResources().getStringArray(R.array.pizzas));
+//        setListAdapter(adapter);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+
+        RecyclerView pizzaRecyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_pizza, container, false);
+        String[] pizzaNames = new String[Pizza.pizzas.length];
+        for (int i = 0; i < pizzaNames.length; i++){
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+        }
+
+        int[] pizzaImages = new int[Pizza.pizzas.length];
+        for (int i = 0; i < pizzaImages.length; i++){
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames,pizzaImages);
+        pizzaRecyclerView.setAdapter(adapter);
+        GridLayoutManager grid = new GridLayoutManager(getActivity(), 2);
+        pizzaRecyclerView.setLayoutManager(grid);
+        return pizzaRecyclerView;
     }
 
 }
